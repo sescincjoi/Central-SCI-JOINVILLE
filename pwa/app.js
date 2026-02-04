3b6ef1f2a62f9aadc75a2fcf76e24302f68b95f5
sescincjoi <sescincjoi@gmail.com>
2026-02-03 22:22:38 -0300

    Update app.js

--- pwa/app.js
+++ pwa/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./pwa/sw.js')
+  navigator.serviceWorker.register('./sw.js')
     .then(() => console.log('✅ Service Worker registrado'))
     .catch(err => console.log('❌ Falha ao registrar SW', err));
 }
