cb7305a8dcfc75ffaffce05a1e207faa9104860f
sescincjoi <sescincjoi@gmail.com>
2026-02-05 18:08:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   if (event.request.mode === 'navigate') {
     event.respondWith(
-      caches.match('./index.html')
+      fetch(event.request).catch(() => caches.match('./index.html'))
     );
     return;
   }
