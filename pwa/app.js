7499425262e5795bf25599c76b10c4aef309d22b
sescincjoi <sescincjoi@gmail.com>
2026-02-05 18:13:54 -0300

    Update app.js

--- pwa/app.js
+++ pwa/app.js
@@ -1,5 +1,7 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
+  navigator.serviceWorker.register('/Central-SCI/sw.js', {
+  scope: '/Central-SCI/'
+})
     .then(() => console.log('✅ Service Worker registrado'))
     .catch(err => console.log('❌ Falha ao registrar SW', err));
 }
